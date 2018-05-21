caea502b4f6e98b3975d9f7213a8254ebf709b12
Roma Chuprina <chuprina.r.v@gmail.com>
2018-05-21 13:16:21 +0300

    UI Так сяк зделана

---
+++ Novel/Control/GamePanel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Novel.Control
+{
+    public partial class GamePanel : UserControl
+    {
+        public GamePanel()
+        {
+            InitializeComponent();
+        }
+
+        private void GamePanel_Load(object sender, EventArgs e)
+        {
+
+                
+        }
+    }
+}
